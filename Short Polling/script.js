9112f6f88c79e4789b97a8f78963f8b92a7643ce
AtharvaEpam <atharvamukul_mujumdar@epam.com>
2025-02-17 12:26:28 +0530

    update script.js

--- Short Polling/script.js
+++ Short Polling/script.js
@@ -1,7 +1,7 @@
 const pollInterval = 5000; // 5 seconds
 
 const fetchData = () => {
-  fetch('http://localhost:3000/data')
+  fetch('http://localhost:5500/data')
     .then(response => response.json())
     .then(data => {
       document.getElementById('message').textContent = data.message;
